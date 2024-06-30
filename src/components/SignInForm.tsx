d859fc4ccd337d052606524674bb8a72f16f7db3
FelixShih <zx8714245@gmail.com>
2024-06-30 21:00:26 +0800

    refactor: format the file

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -11,27 +11,27 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { signIn } from '@/app/auth/auth.action';
 
 export const signInSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
+  email: z.string().email(),
+  password: z.string().min(8),
 })
 
 const SignInForm = () => {
   const router = useRouter()
   const form = useForm<z.infer<typeof signInSchema>>({
-      resolver: zodResolver(signInSchema),
-      defaultValues: {
-          email: '',
-          password: ''
-      },
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    },
   })
 
   const onSubmit = async (values: z.infer<typeof signInSchema>) => {
     const res = await signIn(values);
     if (res.success) {
-        toast.success('Account signin successfully')
-        router.push('/')
+      toast.success('Account signin successfully')
+      router.push('/')
     } else {
-        toast.error(res.error)
+      toast.error(res.error)
     }
   }
 
@@ -47,42 +47,42 @@ const SignInForm = () => {
         <Form {...form}>
           <form className='flex flex-col gap-2' onSubmit={form.handleSubmit(onSubmit)}>
             <FormField
-                control={form.control}
-                name='email'
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                            <Input
-                                type='email'
-                                placeholder='Enter your email...'
-                                {...field}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type='email'
+                      placeholder='Enter your email...'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                            <Input
-                                type='password'
-                                placeholder="Enter your password..."
-                                {...field}
-                                onChange={(e) => {
-                                    e.target.value = e.target.value.trim();
-                                    field.onChange(e);
-                                }}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type='password'
+                      placeholder="Enter your password..."
+                      {...field}
+                      onChange={(e) => {
+                        e.target.value = e.target.value.trim();
+                        field.onChange(e);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <Button type='submit' className='self-start'>
                 Login
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -3,13 +3,13 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from 'react-hook-form'
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form"
 
 import { z } from "zod"
@@ -32,22 +32,22 @@ export const signUpSchema = z.object({
 const SignUpForm = () => {
   const router = useRouter()
   const form = useForm<z.infer<typeof signUpSchema>>({
-      resolver: zodResolver(signUpSchema),
-      defaultValues: {
-          confirmPassword: '',
-          name: '',
-          email: '',
-          password: ''
-      },
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      confirmPassword: '',
+      name: '',
+      email: '',
+      password: ''
+    },
   })
 
   const onSubmit = async (values: z.infer<typeof signUpSchema>) => {
     const res = await signUp(values);
     if (res.success) {
-        toast.success('Account created successfully')
-        router.push('/')
+      toast.success('Account created successfully')
+      router.push('/')
     } else {
-        toast.error(res.error)
+      toast.error(res.error)
     }
   }
 
@@ -63,79 +63,79 @@ const SignUpForm = () => {
         <Form {...form}>
           <form className='flex flex-col gap-2' onSubmit={form.handleSubmit(onSubmit)}>
             <FormField
-                control={form.control}
-                name='email'
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                            <Input
-                                type='email'
-                                placeholder='Enter your email...'
-                                {...field}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type='email'
+                      placeholder='Enter your email...'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <FormField
-                control={form.control}
-                name='name'
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Name</FormLabel>
-                        <FormControl>
-                            <Input
-                                placeholder='Enter your name...'
-                                {...field}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder='Enter your name...'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                            <Input
-                                type='password'
-                                placeholder="Enter your password..."
-                                {...field}
-                                onChange={(e) => {
-                                    e.target.value = e.target.value.trim();
-                                    field.onChange(e);
-                                }}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type='password'
+                      placeholder="Enter your password..."
+                      {...field}
+                      onChange={(e) => {
+                        e.target.value = e.target.value.trim();
+                        field.onChange(e);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <FormField
-                control={form.control}
-                name="confirmPassword"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Confirm password</FormLabel>
-                        <FormControl>
-                            <Input
-                                type='password'
-                                placeholder="Please confirm your password"
-                                {...field}
-                                onChange={(e) => {
-                                    e.target.value = e.target.value.trim();
-                                    field.onChange(e);
-                                }}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type='password'
+                      placeholder="Please confirm your password"
+                      {...field}
+                      onChange={(e) => {
+                        e.target.value = e.target.value.trim();
+                        field.onChange(e);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <Button type='submit' className='self-start'>
                 Sign Up
