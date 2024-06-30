9af3ae0579e65bcaa7b39e5e9a2d441b87f8305d
FelixShih <zx8714245@gmail.com>
2024-06-30 20:30:40 +0800

    fix: the wrong typo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = async () => {
           <a className="mr-6 flex items-center space-x-2" href="/">
             <span className="font-bold">SupaTodo</span>
           </a>
-          <Link href="/dashbaord">Dashboard</Link>
+          <Link href="/dashboard">Dashboard</Link>
         </nav>
         <div className="flex flex-1 items-center justify-end space-x-2">
           {user !== null ? (
